073adc6b67cd76fff45be0710be01f5b1de5c5bc
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-09-07 01:16:23 +0530

    refactor code

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from random import randrange,randint,choice
 import time
 from scipy.spatial import Delaunay
 
-side = 2000
-
 def random_gradient(side):
 	img = Image.new("RGB", (side,side), "#FFFFFF")
 	draw = ImageDraw.Draw(img)
@@ -19,44 +17,41 @@ def random_gradient(side):
 
 	return img
 
-img = random_gradient(side)
+def genPoints(radius, xor, yor, qty, side):
+	rX = (xor,side)
+	rY = (yor,side)
 
-radius = 200
-rX = (0,side)
-rY = (0,side)
-qty = 200
+	deltas = set()
+	for x in range(-radius,radius+1):
+		for y in range(-radius, radius+1):
+			if x**2 + y**2 <= radius**2:
+				deltas.add((x,y)) # populate with all possible points within radius
 
-deltas = set()
-for x in range(-radius,radius+1):
-	for y in range(-radius, radius+1):
-		if x**2 + y**2 <= radius**2:
-			deltas.add((x,y))
+	excluded = set()
+	randPoints = []
+	i = 0
 
-randPoints = []
-excluded = set()
+	while i<=qty:
+		x = randrange(*rX)
+		y = randrange(*rY)
 
-i = 0
+		if (x,y) in excluded:
+			continue
+		randPoints.append((x,y)) # add a point
+		i+=1
+		excluded.update((x+dx,y+dx) for (dx,dy) in deltas) # update all points inside circumference
 
-while i<=qty:
-	x = randrange(*rX)
-	y = randrange(*rY)
-
-	if (x,y) in excluded:
-		continue
-	randPoints.append((x,y))
-	i+=1
-	excluded.update((x+dx,y+dx) for (dx,dy) in deltas)
-
-tri = Delaunay(randPoints)
-points = tri.points[tri.simplices]
+	tri = Delaunay(randPoints) # calculate D triangulation of points
+	points = tri.points[tri.simplices] # find all groups of points
+	return points
 
 def calcCenter(ps):
 	mid1 = ((ps[0][0]+ps[1][0])/2, (ps[0][1]+ps[1][1])/2)
 	mid = ((mid1[0]+ps[2][0])/2, (mid1[1]+ps[2][1])/2)
 	return mid
 
-
-def genWall(points, img, rot=False):
+def genWall(points, side):
+	img = random_gradient(side)
 	idata = img.load()
 	draw = ImageDraw.Draw(img)
 	for p in points:
@@ -64,8 +59,11 @@ def genWall(points, img, rot=False):
 		c = idata[calcCenter(tp)]
 		# draw.polygon(tp, outline="#2c2c2c")
 		draw.polygon(tp, fill=c)
+	return img
 
-genWall(points, img)
+side = 2000
+points = genPoints(200, 0, 0, 100, side)
+img = genWall(points, side)
 
 img.show()
 
