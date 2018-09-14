40ad533feb2fa00b7edb9e1ebbb220afa770758f
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-09-14 20:41:01 +0530

    basic frontend

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-from flask import Flask, request, send_file, url_for, redirect
+from flask import Flask, request, send_file, url_for, redirect, render_template
 import os
 import wallgen
 from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__, static_url_path="/static")
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods=['GET','POST'])
 def index():
-    if request.method == 'GET':
-        if request.args['side']:
-            side = int(request.args['side'])
+    if request.method == 'POST':
+        print(request.form)
+        print(request.form['side'])
+        if request.form['side']:
+            side = int(request.form['side'])
             shift = side//10
             side += shift*2
             points = wallgen.genPoints(100,100,side)
             img = wallgen.genWall(points, side, shift)
             img.save('static/wall.png')
             return redirect(url_for('static', filename='wall.png'))
-        else:
-            return "403"
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
