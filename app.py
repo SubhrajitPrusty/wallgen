413cd5ab0f65e74795f07d5531a277027b2e5195
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-09-14 19:57:31 +0530

    changed some values

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, send_file
+from flask import Flask, request, send_file
 import os
 import wallgen
 from gevent.pywsgi import WSGIServer
@@ -9,16 +9,14 @@ app = Flask(__name__)
 def index():
     if request.method == 'GET':
         if request.args['side']:
-            print("HERE")
             side = int(request.args['side'])
             shift = side//10
             side += shift*2
-            points = wallgen.genPoints(200,100,side)
+            points = wallgen.genPoints(100,100,side)
             img = wallgen.genWall(points, side, shift)
             img.save('wall.png')
             return send_file(os.path.abspath('wall.png'), mimetype="image/png")
         else:
-            print("no, HERE")
             return "403"
 
 if __name__ == '__main__':
