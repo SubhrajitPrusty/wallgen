21faf1fe6fe92c1070d7d28b10e9d5db8c5b2b5c
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-09-23 17:32:58 +0530

    Added a new route for API purpose

--- app.py
+++ app.py
@@ -8,53 +8,99 @@ app = Flask(__name__, static_url_path="/static")
 
 @app.route("/", methods=['GET','POST'])
 def index():
-    if request.method == 'POST':
-        print(request.form)
-        if request.form['side'] and request.form['np'] and (request.form['rgb1'] and request.form['rgb2']):
-            side = int(request.form['side'])
-            np = int(request.form['np'])
-            
-            rgb1 = request.form['rgb1'][1:]
-            rgb2 = request.form['rgb2'][1:]
+	if request.method == 'POST':
+		print(request.form)
+		if request.form['side'] and request.form['np'] and (request.form['rgb1'] and request.form['rgb2']):
+			side = int(request.form['side'])
+			np = int(request.form['np'])
+			
+			rgb1 = request.form['rgb1'][1:]
+			rgb2 = request.form['rgb2'][1:]
 
-            error = None
-            
-            try:
-                rgb1 = tuple(bytes.fromhex(rgb1))
-                rgb2 = tuple(bytes.fromhex(rgb2))
-            except Exception as e:
-                if e:
-                    error = "ERROR: Invalid color hex"
-            
-            print(side, np, rgb1, rgb2)
-            
-            if (side > 4000 and side >= 100) or side < 100:
-                error = "WARNING: Image too large OR Image too small"
-            if np < 10 or np > 300:
-                error = "WARNING: Too less points OR too many points"
+			error = None
+			
+			try:
+				rgb1 = tuple(bytes.fromhex(rgb1))
+				rgb2 = tuple(bytes.fromhex(rgb2))
+			except Exception as e:
+				if e:
+					error = "ERROR: Invalid color hex"
+			
+			print(side, np, rgb1, rgb2)
+			
+			if (side > 4000 and side >= 100) or side < 100:
+				error = "WARNING: Image too large OR Image too small"
+			if np < 10 or np > 300:
+				error = "WARNING: Too less points OR too many points"
 
-            if error != None:
-                print(error)
-                return render_template('error.html', context=error)
-            else:
-                fname = "wall-{}.png".format(int(time.time()))
-                fpath = 'static/images/'+fname
-                shift = side//10
-                side += shift*2
-                points = wallgen.genPoints(np, side)
-                img = wallgen.gradient(side, rgb1, rgb2)
-                img = wallgen.genWall(img, points, side, shift)
-                print(fpath)
-                img.save(fpath)
-                imgurl = url_for('static',filename='images/'+fname)
-                return render_template("download.html", context=imgurl)
-        else:
-            error = "Invalid input, try again"
-            return render_template("error.html", context=error)
-    else:
-        return render_template('index.html')
+			if error != None:
+				print(error)
+				return render_template('error.html', context=error)
+			else:
+				fname = "wall-{}.png".format(int(time.time()))
+				fpath = 'static/images/'+fname
+				shift = side//10
+				side += shift*2
+				points = wallgen.genPoints(np, side)
+				img = wallgen.gradient(side, rgb1, rgb2)
+				img = wallgen.genWall(img, points, side, shift)
+				print(fpath)
+				img.save(fpath)
+				imgurl = url_for('static',filename='images/'+fname)
+				return render_template("download.html", context=imgurl)
+		else:
+			error = "Invalid input, try again"
+			return render_template("error.html", context=error)
+	else:
+		return render_template('index.html')
+
+
+
+@app.route("/download", methods=['POST'])
+def download():
+	if request.method == 'POST':
+		print(request.form)
+		if request.form['side'] and request.form['np'] and (request.form['rgb1'] and request.form['rgb2']):
+			side = int(request.form['side'])
+			np = int(request.form['np'])
+			
+			rgb1 = request.form['rgb1'][1:]
+			rgb2 = request.form['rgb2'][1:]
+
+			error = None
+			
+			try:
+				rgb1 = tuple(bytes.fromhex(rgb1))
+				rgb2 = tuple(bytes.fromhex(rgb2))
+			except Exception as e:
+				if e:
+					error = "ERROR: Invalid color hex"
+			
+			print(side, np, rgb1, rgb2)
+			
+			if (side > 4000 and side >= 100) or side < 100:
+				error = "WARNING: Image too large OR Image too small"
+			if np < 10 or np > 300:
+				error = "WARNING: Too less points OR too many points"
+
+			if error != None:
+				print(error)
+				return error, 403
+			else:
+				fname = "wall-{}.png".format(int(time.time()))
+				fpath = 'static/images/'+fname
+				shift = side//10
+				side += shift*2
+				points = wallgen.genPoints(np, side)
+				img = wallgen.gradient(side, rgb1, rgb2)
+				img = wallgen.genWall(img, points, side, shift)
+				print(fpath)
+				img.save(fpath)
+				imgurl = url_for('static',filename='images/'+fname)
+				return send_file(fpath, mimetype="image/png")
+
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    http_server = WSGIServer(('',port),app)
-    http_server.serve_forever()
+	port = int(os.environ.get('PORT', 5000))
+	http_server = WSGIServer(('',port),app)
+	http_server.serve_forever()
