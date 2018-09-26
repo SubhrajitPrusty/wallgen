90a53310f4cc307917817c9d8ff179265a3409c5
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-09-26 21:33:27 +0530

    Added (modified) old method as an option, updated README and demo

--- README.md
+++ README.md
@@ -8,22 +8,58 @@ Generates HQ poly wallpapers
 
 ## Usage
 
-```
-Usage: wallgen [OPTIONS] SIDE
+### `wallgen`
 
-  Generates a side X side HQ low poly image of random gradient
+```
+Usage: wallgen [OPTIONS] COMMAND [ARGS]...
+
+Options:
+  --help  Show this message and exit.
+
+Commands:
+  pattern  Generate a HQ image of a beautiful pattern
+  poly     Generates a HQ low poly image
+```
+
+### `wallgen poly --help`
+
+```
+Usage: wallgen poly [OPTIONS] SIDE
+
+  Generates a HQ low poly image
 
 Options:
   --colors TEXT...  use color1 --> color2 gradient, e.g #ff0000 #0000ff
   --np INTEGER      number of points to use, default = 100
-  --radius INTEGER  radius, within which no other point is generated,
-                    default=200
+  --show            open the image
+  --help            Show this message and exit.
+```
+
+### `wallgen pattern --help`
+
+
+```
+Usage: wallgen pattern [OPTIONS] SIDE
+
+  Generate a HQ image of a beautiful pattern
+
+Options:
+  --sq              use squares instead of rhombus
+  --colors TEXT...  use color1 --> color2 gradient, e.g #ff0000 #0000ff
   --show            open the image
   --help            Show this message and exit.
 ```
 
 ## Examples
 
+## `wallgen poly 2000 --colors #dd0000 #4455ff`
+
 ![](./images/demo1.png)
+
+## `wallgen poly 2000`
+
 ![](./images/demo2.png)
+
+## `wallgen pattern 2000 --sq --colors #dd0000 #4455ff`
+
 ![](./images/demo3.png)
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sys
 from PIL import Image, ImageDraw
 from random import randrange,randint
 import time
@@ -33,7 +34,8 @@ def gradient(side, rgb1, rgb2):
 	return img
 
 
-def genPoints(radius, qty, side):
+def genPoints(qty, side):
+	radius = side // 20
 	rX = (0,side)
 	rY = (0,side)
 
@@ -71,24 +73,110 @@ def genWall(img, points, side, shift):
 	draw = ImageDraw.Draw(img)
 	for p in points:
 		tp = tuple(map(tuple,p))
-		c = idata[calcCenter(tp)]
+		c = (255,255,255)
+		try:
+			c = idata[calcCenter(tp)]
+		except Exception as e:
+			pass
 		# draw.polygon(tp, outline="#2c2c2c")
 		draw.polygon(tp, fill=c)
 	img = img.crop((shift,shift,side-shift,side-shift))
 	return img
 
-@click.command()
+def genPattern(x, y, side, boxes, img, square=False):
+	idata = img.load()
+	draw = ImageDraw.Draw(img)
+	inc = side//boxes
+	xback = x
+	mult = 2
+	boxes = boxes//mult
+	for i in range(boxes):
+		for j in range(boxes):
+			if square:
+				points = [(x,y),(x,y+inc),(x+inc,y+inc),(x+inc,y)] # squares
+			else:
+				points = [(x,y),(x-inc,y+inc),(x,y+2*inc),(x+inc,y+inc)] #rhombus
+
+			a,b = x+inc,y+inc
+			a = a if a>0 else 0
+			a = a if a<side else a-2*inc
+			b = b if b>0 else 0
+			b = b if b<side else b-2*inc
+			mx = (a,b)
+			c = idata[mx]
+
+			draw.polygon((points), fill=tuple(c))
+			x+=mult*inc
+	
+		y+=mult*inc
+		x=xback
+
+	return img
+
+
+@click.group()
+
+def cli():
+	pass
+
+@cli.command()
 @click.argument("side", type=click.INT)
+# @click.option("--pic",type=click.Path(exists=True,dir_okay=False),help="Use a pic instead of gradient background")
 @click.option("--colors", nargs=2, type=click.STRING, help="use color1 --> color2 gradient, e.g #ff0000 #0000ff")
 @click.option("--np", default=100, help="number of points to use, default = 100")
-@click.option("--radius", default=200, help="radius, within which no other point is generated, default=200")
 @click.option("--show", is_flag=True, help="open the image")
-def cli(side,np,radius,show,colors):
-	""" Generates a side X side HQ low poly image of random gradient """
+
+def poly(side, np, show, colors):
+	""" Generates a HQ low poly image """
+
+	error = ""
+	if side < 50:
+		error = "Image too small. Minimum size 50"
+	elif np < 3:
+		error = "Too less points. Minimum points 3"
+	elif np > side//2:
+		error = "Too many points. Maximum points {}".format(side//2)
+
+	if error:
+		click.echo(error)
+		sys.exit(1)
 
 	shift = side//10
 	side += shift*2
 	
+	# if pic:
+	# 	img = img.load(pic)
+	if colors:
+		rgb1 = tuple(bytes.fromhex(colors[0][1:]))
+		rgb2 = tuple(bytes.fromhex(colors[1][1:]))
+		img = gradient(side, rgb1, rgb2)
+	else:
+		img = random_gradient(side)
+
+	points = genPoints(np, side)
+	img = genWall(img, points, side, shift)
+
+	if show:
+		img.show()
+
+	img.save("wall-{}.png".format(int(time.time())))
+
+@cli.command()
+@click.argument("side", type=click.INT)
+@click.option("--sq", is_flag=True, help="use squares instead of rhombus")
+@click.option("--colors", nargs=2, type=click.STRING, help="use color1 --> color2 gradient, e.g #ff0000 #0000ff")
+@click.option("--show", is_flag=True, help="open the image")
+def pattern(side, colors, show, sq):
+	""" Generate a HQ image of a beautiful pattern """
+
+	error = ""
+	if side < 50:
+		error = "Image too small. Minimum size 50"
+
+	if error:
+		click.echo(error)
+		sys.exit(1)
+
 	if not colors:
 		img = random_gradient(side)
 	else:
@@ -96,8 +184,10 @@ def cli(side,np,radius,show,colors):
 		rgb2 = tuple(bytes.fromhex(colors[1][1:]))
 		img = gradient(side, rgb1, rgb2)
 
-	points = genPoints(radius, np, side)
-	img = genWall(img, points, side, shift)
+	boxes = side // 100 + 2 # this config looks good
+	img = genPattern(0, 0, side, boxes, img, sq)
+	temp = side//boxes
+	img = genPattern(temp, temp, side, boxes, img, sq)
 
 	if show:
 		img.show()
