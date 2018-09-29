15f754ab2cd0fcb44c525806c4d22625f983491a
Subhrajit Prusty <subhrajit1997@gmail.com>
2018-09-30 01:55:23 +0530

    added pattern images, added new homepage

--- app.py
+++ app.py
@@ -6,8 +6,13 @@ from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__, static_url_path="/static")
 
-@app.route("/", methods=['GET','POST'])
+@app.route("/", methods=['GET'])
 def index():
+	return render_template("index.html")
+
+
+@app.route("/poly", methods=['GET','POST'])
+def poly():
 	if request.method == 'POST':
 		print(request.form)
 		if request.form['side'] and request.form['np'] and (request.form['rgb1'] and request.form['rgb2']):
@@ -52,7 +57,54 @@ def index():
 			error = "Invalid input, try again"
 			return render_template("error.html", context=error)
 	else:
-		return render_template('index.html')
+		return render_template('poly.html')
+
+@app.route("/pattern", methods=['GET','POST'])
+def pattern():
+	if request.method == 'POST':
+		print(request.form)
+		if request.form['side'] and (request.form['rgb1'] and request.form['rgb2']):
+			side = int(request.form['side'])
+			sq = request.form.get('squares')
+			print(sq)
+			
+			rgb1 = request.form['rgb1'][1:]
+			rgb2 = request.form['rgb2'][1:]
+
+			error = None
+			
+			try:
+				rgb1 = tuple(bytes.fromhex(rgb1))
+				rgb2 = tuple(bytes.fromhex(rgb2))
+			except Exception as e:
+				if e:
+					error = "ERROR: Invalid color hex"
+			
+			print(side, sq, rgb1, rgb2)
+			
+			if (side > 4000 and side >= 100) or side < 100:
+				error = "WARNING: Image too large OR Image too small"
+
+			if error != None:
+				print(error)
+				return render_template('error.html', context=error)
+			else:
+				fname = "wall-{}.png".format(int(time.time()))
+				fpath = 'static/images/'+fname
+				img = wallgen.gradient(side, rgb1, rgb2)
+				boxes = side // 100 + 2 # this config looks good
+				img = wallgen.genPattern(0, 0, side, boxes, img, sq)
+				temp = side//boxes
+				img = wallgen.genPattern(temp, temp, side, boxes, img, sq)
+				print(fpath)
+				img.save(fpath)
+				imgurl = url_for('static',filename='images/'+fname)
+				return render_template("download.html", context=imgurl)
+		else:
+			error = "Invalid input, try again"
+			return render_template("error.html", context=error)
+	else:
+		return render_template('pattern.html')
 
 
 
--- wallgen.py
+++ wallgen.py
@@ -1,3 +1,4 @@
+import sys
 from PIL import Image, ImageDraw
 from random import randrange,randint
 import time
@@ -34,7 +35,7 @@ def gradient(side, rgb1, rgb2):
 
 
 def genPoints(qty, side):
-	radius = side // 20
+	radius = side // 20 # radius is set to 5% - good config
 	rX = (0,side)
 	rY = (0,side)
 
@@ -63,33 +64,120 @@ def genPoints(qty, side):
 	return points
 
 def calcCenter(ps):
+	""" calculate incenter of a triangle given all vertices"""
 	mid1 = ((ps[0][0]+ps[1][0])/2, (ps[0][1]+ps[1][1])/2)
 	mid = ((mid1[0]+ps[2][0])/2, (mid1[1]+ps[2][1])/2)
 	return mid
 
 def genWall(img, points, side, shift):
-	idata = img.load()
+	idata = img.load() # load pixel data
 	draw = ImageDraw.Draw(img)
 	for p in points:
-		tp = tuple(map(tuple,p))
-		c = idata[calcCenter(tp)]
+		tp = tuple(map(tuple,p)) # convert each pair of points to tuples
+		c = (255,255,255) # default color incase of exception
+		try:
+			c = idata[calcCenter(tp)]
+		except Exception as e:
+			pass
 		# draw.polygon(tp, outline="#2c2c2c")
-		draw.polygon(tp, fill=c)
-	img = img.crop((shift,shift,side-shift,side-shift))
+		draw.polygon(tp, fill=c) # draw one triangle
+	img = img.crop((shift,shift,side-shift,side-shift)) # crop back to normal size
+
 	return img
 
-@click.command()
+def genPattern(x, y, side, boxes, img, square=False):
+	idata = img.load() # load pixel data
+	draw = ImageDraw.Draw(img) 
+	inc = side//boxes #increment size
+	xback = x # backup of x
+	mult = 2 
+	boxes = boxes//mult # adjustment
+	for i in range(boxes):
+		for j in range(boxes):
+			if square:
+				points = [(x,y),(x,y+inc),(x+inc,y+inc),(x+inc,y)] # squares
+			else:
+				points = [(x,y),(x-inc,y+inc),(x,y+2*inc),(x+inc,y+inc)] #rhombus
+
+			a,b = x+inc,y+inc # to get pixel data
+			a = a if a>0 else 0 # prevent underflow
+			a = a if a<side else a-2*inc # prevent overflow
+			b = b if b>0 else 0 # prevent underflow
+			b = b if b<side else b-2*inc # prevent overflow
+			c = idata[a,b] # color data
+
+			draw.polygon((points), fill=tuple(c)) # draw one polygon
+			x+=mult*inc # shift cursor horizontally
+	
+		y+=mult*inc # shift cursor vertically
+		x=xback # restore horizontal starting point
+
+	return img # return final image
+
+
+@click.group()
+
+def cli():
+	pass
+
+@cli.command()
 @click.argument("side", type=click.INT)
-@click.option("--colors", nargs=2, type=click.STRING, help="use color1 --> color2 gradient, e.g #ff0000 #0000ff")
+# @click.option("--pic",type=click.Path(exists=True,dir_okay=False),help="Use a pic instead of gradient background")
+@click.option("--colors", nargs=2, type=click.STRING, help="use custom gradient, e.g --colors #ff0000 #0000ff")
 @click.option("--np", default=100, help="number of points to use, default = 100")
-@click.option("--radius", default=200, help="radius, within which no other point is generated, default=200")
 @click.option("--show", is_flag=True, help="open the image")
-def cli(side,np,radius,show,colors):
-	""" Generates a side X side HQ low poly image of random gradient """
+
+def poly(side, np, show, colors):
+	""" Generates a HQ low poly image """
+
+	error = ""
+	if side < 50:
+		error = "Image too small. Minimum size 50"
+	elif np < 3:
+		error = "Too less points. Minimum points 3"
+	elif np > side//2:
+		error = "Too many points. Maximum points {}".format(side//2)
+
+	if error:
+		click.echo(error)
+		sys.exit(1)
 
 	shift = side//10
-	side += shift*2
+	side += shift*2 # increase size to prevent underflow
 	
+	# if pic:
+	# 	img = img.load(pic)
+	if colors:
+		rgb1 = tuple(bytes.fromhex(colors[0][1:]))
+		rgb2 = tuple(bytes.fromhex(colors[1][1:]))
+		img = gradient(side, rgb1, rgb2)
+	else:
+		img = random_gradient(side)
+
+	points = genPoints(np, side)
+	img = genWall(img, points, side, shift)
+
+	if show:
+		img.show()
+
+	img.save("wall-{}.png".format(int(time.time())))
+
+@cli.command()
+@click.argument("side", type=click.INT)
+@click.option("--sq", is_flag=True, help="use squares instead of rhombus")
+@click.option("--colors", nargs=2, type=click.STRING, help="use custom gradient, e.g --colors #ff0000 #0000ff")
+@click.option("--show", is_flag=True, help="open the image")
+def pattern(side, colors, show, sq):
+	""" Generate a HQ image of a beautiful pattern """
+
+	error = ""
+	if side < 50:
+		error = "Image too small. Minimum size 50"
+
+	if error:
+		click.echo(error)
+		sys.exit(1)
+
 	if not colors:
 		img = random_gradient(side)
 	else:
@@ -97,8 +185,10 @@ def cli(side,np,radius,show,colors):
 		rgb2 = tuple(bytes.fromhex(colors[1][1:]))
 		img = gradient(side, rgb1, rgb2)
 
-	points = genPoints(radius, np, side)
-	img = genWall(img, points, side, shift)
+	boxes = side // 100 + 2 # this config looks good
+	img = genPattern(0, 0, side, boxes, img, sq)
+	temp = side//boxes
+	img = genPattern(temp, temp, side, boxes, img, sq)
 
 	if show:
 		img.show()
