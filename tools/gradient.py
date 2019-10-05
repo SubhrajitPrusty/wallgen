f25fad5d42af467b43fadc0c3595e1a6bf3775f1
Priyam Harsh <42106642+priyamharsh14@users.noreply.github.com>
2019-10-05 10:19:14 +0530

    Update gradient.py

--- tools/gradient.py
+++ tools/gradient.py
@@ -95,12 +95,12 @@ def swirl_image(image):
 
 def is_64_windows():
 	"""Find out how many bits is OS. """
-    return struct.calcsize('P') * 8 == 64
+	return struct.calcsize('P') * 8 == 64
 
 def get_sys_parameters_info():
 	"""Based on if this is 32bit or 64bit returns correct version of SystemParametersInfo function. """
-    return ctypes.windll.user32.SystemParametersInfoW if is_64_windows() \
-    	else ctypes.windll.user32.SystemParametersInfoA
+	return ctypes.windll.user32.SystemParametersInfoW if is_64_windows() \
+		else ctypes.windll.user32.SystemParametersInfoA
 
 def set_wallpaper(file_name, flag=False):
 	if sys.platform == 'win32':
